3cce021c591f6f24bac812b170a14a82ea18ce4e
Haniel Costa da Silva <hanielcosta.ti@gmail.com>
2023-07-19 15:13:43 -0300

    Updates

---
+++ precache-manifest.dd4d5a05b595284f79daf438c8c47d2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d6dfac204e8d43468dd",
+    "url": "/chronometer/static/css/main.66001b5e.chunk.css"
+  },
+  {
+    "revision": "2d6dfac204e8d43468dd",
+    "url": "/chronometer/static/js/main.2d6dfac2.chunk.js"
+  },
+  {
+    "revision": "6eb4b3af0436ca46f4c8",
+    "url": "/chronometer/static/js/1.6eb4b3af.chunk.js"
+  },
+  {
+    "revision": "4d7e50ec578c583c8cbd",
+    "url": "/chronometer/static/js/runtime~main.4d7e50ec.js"
+  },
+  {
+    "revision": "7acfa456b00d892b5bc2326cc1fe1320",
+    "url": "/chronometer/static/media/chronometer.7acfa456.png"
+  },
+  {
+    "revision": "06b1e619a18ee6f59553dd072d82be19",
+    "url": "/chronometer/index.html"
+  }
+];
